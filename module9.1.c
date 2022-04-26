3d17b8a528fffc79413a70e1abf4b3eda5c47172
zilani131 <zilani.cuet@gmail.com>
2022-04-26 13:00:09 +0600

    constant and variable important topic

--- module9.1.c
+++ module9.1.c
@@ -1,4 +1,13 @@
 #include<stdio.h>
 int main(){
-    
+//  when we use constant we have to assign the value of 
+// constant when we declare it other wise we can not assign it in future;
+// Constant:
+// const int PI =3.5;
+// variable:
+// int a;
+// a=5; we can first declare it and then assign the value 
+// int a=5;
+// we can declare the variable and assign it in the same  line 
+
 }
